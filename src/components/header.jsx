49847503869d2f23d5ee354d37330b21b39ce05c
Alexey <89641234561@mail.ru>
2020-06-27 08:34:08 +0800

    Разбивает макет на большие компоненты

---
+++ src/components/header.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "../App.css";
+
+const Header = () => {
+    return (
+        <header className="header"><img src="https://www.gettyimages.pt/gi-resources/images/Homepage/Hero/PT/PT_hero_42_153645159.jpg" width="30px" height="30px" alt="logo"></img></header>
+    );
+};
+
+export default Header;
