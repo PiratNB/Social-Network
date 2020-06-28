de08491ac996629760d6e711b4ae5d78bd442b77
Alexey <89641234561@mail.ru>
2020-06-28 22:12:07 +0800

    Заполняет компонент диалогов

--- src/components/dialogs/dialogs.jsx
+++ src/components/dialogs/dialogs.jsx
@@ -1,9 +1,38 @@
 import React from "react";
-// import c from "./dialogs.module.css";
+import c from "./dialogs.module.css";
 
 const Dialogs = () => {
     return (
-        <p>Dialogs</p>
+        <div className={c.dialogs}>
+            <div className={c.dialogsItems}>
+                <div className={c.dialog + ` `+ c.active}>
+                    Alexey
+                </div>
+                <div className={c.dialog}>
+                    Daha
+                </div>
+                <div className={c.dialog}>
+                    Marina
+                </div>
+                <div className={c.dialog}>
+                    Dmitriy
+                </div>
+                <div className={c.dialog}>
+                    Sasha
+                </div>
+            </div>
+            <div className={c.messages}>
+                <div className={c.message}>
+                    Привет!
+                </div>
+                <div className={c.message}>
+                    Привет, как дела?
+                </div>
+                <div className={c.message}>
+                    Привет, мы знакомы?
+                </div>
+            </div>
+        </div>
     );
 };
 
