75e04c61f4aedd8d43b7a8c43cc46713f17a4f23
Alexey <89641234561@mail.ru>
2020-06-29 09:46:33 +0800

    Добавляет NavLink для имен диалогов

--- src/components/dialogs/dialogs.jsx
+++ src/components/dialogs/dialogs.jsx
@@ -1,39 +1,34 @@
 import React from "react";
 import c from "./dialogs.module.css";
+import { NavLink } from "react-router-dom";
 
 const Dialogs = () => {
-    return (
-        <div className={c.dialogs}>
-            <div className={c.dialogsItems}>
-                <div className={c.dialog + ` `+ c.active}>
-                    Alexey
-                </div>
-                <div className={c.dialog}>
-                    Daha
-                </div>
-                <div className={c.dialog}>
-                    Marina
-                </div>
-                <div className={c.dialog}>
-                    Dmitriy
-                </div>
-                <div className={c.dialog}>
-                    Sasha
-                </div>
-            </div>
-            <div className={c.messages}>
-                <div className={c.message}>
-                    Привет!
-                </div>
-                <div className={c.message}>
-                    Привет, как дела?
-                </div>
-                <div className={c.message}>
-                    Привет, мы знакомы?
-                </div>
-            </div>
+  return (
+    <div className={c.dialogs}>
+      <div className={c.dialogsItems}>
+        <div className={c.dialog + ` ` + c.active}>
+          <NavLink to="/dialogs/1">Alexey</NavLink>
         </div>
-    );
+        <div className={c.dialog}>
+          <NavLink to="/dialogs/2">Daha</NavLink>
+        </div>
+        <div className={c.dialog}>
+          <NavLink to="/dialogs/3">Marina</NavLink>
+        </div>
+        <div className={c.dialog}>
+          <NavLink to="/dialogs/4">Dmitriy</NavLink>
+        </div>
+        <div className={c.dialog}>
+          <NavLink to="/dialogs/5">Sasha</NavLink>
+        </div>
+      </div>
+      <div className={c.messages}>
+        <div className={c.message}>Привет!</div>
+        <div className={c.message}>Привет, как дела?</div>
+        <div className={c.message}>Привет, мы знакомы?</div>
+      </div>
+    </div>
+  );
 };
 
 export default Dialogs;
