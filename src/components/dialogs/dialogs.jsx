45f70899f18c80530780319159c4d751c36bd8a8
Alexey <89641234561@mail.ru>
2020-07-07 20:01:00 +0800

    Добавляет колбек onClick через реф ссылки в компоненты диалогов и постов

--- src/components/dialogs/dialogs.jsx
+++ src/components/dialogs/dialogs.jsx
@@ -4,17 +4,31 @@ import Dialog from "./dialog/dialog";
 import Message from "./message/message";
 
 const Dialogs = (props) => {
-  
-  let dialogsElements = props.state.dialogs.map(d => <Dialog name={d.name} id={d.id} />);
-  let messagesElements = props.state.messages.map(m => <Message text={m.text} />);
+  let dialogsElements = props.state.dialogs.map((d) => (
+    <Dialog name={d.name} id={d.id} />
+  ));
+  let messagesElements = props.state.messages.map((m) => (
+    <Message text={m.text} />
+  ));
+
+  let newDialogElement = React.createRef();
+
+  let addDialog = () => {
+    let text = newDialogElement.current.value;
+    alert(text);
+  };
 
   return (
     <div className={c.dialogs}>
-      <div className={c.dialogsItems}>
-        {dialogsElements}
-      </div>
-      <div className={c.messages}>
-        {messagesElements}
+      <div className={c.dialogsItems}>{dialogsElements}</div>
+      <div>
+        <div className={c.messages}>{messagesElements}</div>
+        <div>
+          <textarea ref={newDialogElement}></textarea>
+        </div>
+        <div>
+          <button onClick={addDialog}>Send</button>
+        </div>
       </div>
     </div>
   );
--- src/components/profile/myPosts/myPosts.jsx
+++ src/components/profile/myPosts/myPosts.jsx
@@ -3,20 +3,30 @@ import Post from "./post/post";
 import c from "./myPosts.module.css";
 
 const MyPosts = (props) => {
-
-  let postsElements = props.posts.map(p => (
+  let postsElements = props.posts.map((p) => (
     <Post message={p.message} likesCount={p.likesCount} />
   ));
+  
+  let newPostElement = React.createRef();
+
+  let addPost = () => {
+    let text = newPostElement.current.value;
+    alert(text);
+  };
 
   return (
     <div>
       <div className={c.postsBlock}>
         <h3>Мои посты:</h3>
         <div>
-          <textarea></textarea>
+          <textarea ref={newPostElement}></textarea>
         </div>
         <div>
-          <button>Add post</button>
+          <button
+            onClick={addPost}
+          >
+            Add post
+          </button>
         </div>
       </div>
       {postsElements}
