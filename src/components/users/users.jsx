266ab32dfbfae1e988d634603a3ba0e8f6eb31d3
Alexey <89641234561@mail.ru>
2020-07-20 16:30:27 +0800

    Устанавливает Axios и создает простой get запрос, отрисовывыет по нему пользователей

--- src/components/users/users.jsx
+++ src/components/users/users.jsx
@@ -1,32 +1,22 @@
 import React from "react";
 import styles from './users.module.css'
-
+import * as Axios from "axios";
+import userPhoto from '../../assets/img/img.jpg'
 let Users = (props) => {
 
     if (props.users.length === 0) {
-        props.setUsers(
-         [
-            {
-                id: 1, photoUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSp--3Vi2GrynzFGgTKgFPQ5V-qKDDcaPMJTg&usqp=CAU',
-                followed: true, fullName: 'Alexey', status: 'hello world', location: { city: 'Irkutsk', country: 'Russia' }
-            },
-            {
-                id: 2, photoUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSp--3Vi2GrynzFGgTKgFPQ5V-qKDDcaPMJTg&usqp=CAU',
-                followed: true, fullName: 'Ivan', status: 'hello world', location: { city: 'Irkutsk', country: 'Russia' }
-            },
-            {
-                id: 3, photoUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSp--3Vi2GrynzFGgTKgFPQ5V-qKDDcaPMJTg&usqp=CAU',
-                followed: false, fullName: 'Dmitriy', status: 'hello world', location: { city: 'Moskow', country: 'Russia' }
-            }
-          ]
-        )
-    }
+
+        Axios.get("https://social-network.samuraijs.com/api/1.0/users").then(Response => {
+            props.setUsers(Response.data.items);
+        });
+
+    };
 
     return <div>
         {props.users.map(u => <div key={u.id}>
             <span>
                 <div>
-                    <img src={u.photoUrl} className={styles.userPhoto} />
+                    <img src={ u.photos.small != null ? u.photos.small: userPhoto} className={styles.userPhoto} />
                 </div>
                 <div>
                     {u.followed ? <button onClick={() => { props.unfollow(u.id) }}>Удалить</button> : <button onClick={() => { props.follow(u.id) }}>Добавить</button>}
@@ -34,12 +24,12 @@ let Users = (props) => {
             </span>
             <span>
                 <span>
-                    <div>{u.fullName}</div>
+                    <div>{u.name}</div>
                     <div>{u.status}</div>
                 </span>
                 <span>
-                    <div>{u.location.country}</div>
-                    <div>{u.location.city}</div>
+                    <div>{"u.location.country"}</div>
+                    <div>{"u.location.city"}</div>
                 </span>
             </span>
         </div>)}
